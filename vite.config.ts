ebfd65e1406c663f9c0a630a562c5fbba5a2b77c
stars-one <31400169+stars-one@users.noreply.github.com>
2024-10-25 14:22:57 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ import markdown from 'vite-plugin-vue-markdown';
 import svgLoader from 'vite-svg-loader';
 import { configDefaults } from 'vitest/config';
 
-const baseUrl = process.env.BASE_URL ?? '/mytools';
+const baseUrl = process.env.BASE_URL ?? '/my-tools';
 
 // https://vitejs.dev/config/
 export default defineConfig({
